21aca87baac84227869dccdaffa8a6d713dc2632
DeepanKumar2553 <ben10alienforce2553@gmail.com>
2024-10-04 14:39:16 +0530

    demo-602

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ const authenticateUser = require('./middleware/authentication')
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.json())
 app.use(helmet())
-app.use(cors())
+app.use(cors({ origin: '*' }))
 app.use(xss())
 
 app.use('/api/v1/authRouter', authRouter)
